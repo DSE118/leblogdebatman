cc3cbbe79721ef697390c73462ac8e53edea189a
David SIMOINE <d118@a.a>
2021-10-08 16:40:19 +0200

    Ajout page profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class MainController extends AbstractController
 {
@@ -16,4 +17,17 @@ class MainController extends AbstractController
     {
         return $this->render('main/home.html.twig');
     }
+
+    /**
+     * Page de profil
+     *
+     * @Route("/mon-profil/", name="main_profil")
+     * @Security ("is_granted('ROLE_USER')")
+     */
+    public function profil(): Response
+    {
+
+        return $this->render('main/profil.html.twig');
+    }
+
 }
