016a58756572f08dd8cc7f0812c55ba3b0b38586
David SIMOINE <d118@a.a>
2021-10-12 16:18:04 +0200

    Ajout page résultat de recherche & MAJ README

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 https://github.com/DSE118/leblogdebatman.git
 ```
 
-### Modifier les paramètres d'environnement dans le fichier .env (changer user_db et password_db)
+### Modifier les paramètres d'environnement dans le fichier .env (changer user_db, password_db, clés Google Recaptcha)
 
 ### Déplacer le terminal dans le dossier cloné
 ```
@@ -21,6 +21,7 @@ symfony console doctrine:database:create
 symfony console make:migration
 symfony console doctrine:migrations:migrate
 symfony console doctrine:fixtures:load
+symfony console assets:install public
 ```
 
 Les fixtures créeront :
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\NewArticleFormType;
 use DateTime;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Component\Pager\PaginatorInterface;
-
 
 /**
  * Préfixes de la route et du nom des pages du blog
@@ -54,7 +53,8 @@ class BlogController extends AbstractController
 
             return $this->redirectToRoute('blog_publication_view', [
                 'slug' => $newArticle->getSlug(),
-                ]);
+            ]);
+
         }
 
         return $this->render('blog/newPublication.html.twig', [
@@ -74,13 +74,13 @@ class BlogController extends AbstractController
         $requestedPage = $request->query->getInt('page', 1);
 
         // Vérification que le numéro est positif
-        if ($requestedPage < 1){
+        if($requestedPage < 1){
             throw new NotFoundHttpException();
         }
 
         $em = $this->getDoctrine()->getManager();
 
-        $query = $em->createQuery('SELECT a FROM App\Entity\Article a ORDER BY a.publicationDate DESC ');
+        $query = $em->createQuery('SELECT a FROM App\Entity\Article a ORDER BY a.publicationDate DESC');
 
         // Récupération des articles
         $articles = $paginator->paginate(
@@ -98,7 +98,7 @@ class BlogController extends AbstractController
     /**
      * Page permettant de voir un article en détail
      *
-     * @Route("/publication/{slug}/", name ="publication_view")
+     * @Route("/publication/{slug}/", name="publication_view")
      */
     public function publicationView(Article $article): Response
     {
@@ -106,6 +106,45 @@ class BlogController extends AbstractController
         return $this->render('blog/publicationView.html.twig', [
             'article' => $article,
         ]);
+    }
+
+    /**
+     * @Route("/recherche/", name="search")
+     */
+    public function search(Request $request, PaginatorInterface $paginator): Response
+    {
+
+        // Récupération du numéro de la page demandée dans l'URL
+        $requestedPage = $request->query->getInt('page', 1);
+
+        // Vérification que le numéro est positif
+        if($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
+
+        $search = $request->query->get('q', '');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em
+            ->createQuery('SELECT a FROM App\Entity\Article a WHERE a.title LIKE :search OR a.content LIKE :search ORDER BY a.publicationDate DESC')
+            ->setParameters([
+                'search' => '%' . $search . '%',
+            ])
+        ;
+
+        // Récupération des articles
+        $articles = $paginator->paginate(
+            $query,
+            $requestedPage,
+            10
+        );
+
+
+        return $this->render('blog/listSearch.html.twig', [
+            'articles' => $articles,
+        ]);
+
     }
 
 }
